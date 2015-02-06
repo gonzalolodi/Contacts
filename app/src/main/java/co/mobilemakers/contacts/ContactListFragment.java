ae8f7d335d967d228b456ec882cc65827b36d7d8
Gonzalo Lodi <gonzalo.lodi@globant.com>
2015-02-06 18:19:05 -0300

    initial commit

---
+++ app/src/main/java/co/mobilemakers/contacts/ContactListFragment.java
@@ -0,0 +1,24 @@
+package co.mobilemakers.contacts;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ContactListFragment extends ListFragment {
+
+    public ContactListFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_contact_list, container, false);
+        return rootView;
+    }
+}
